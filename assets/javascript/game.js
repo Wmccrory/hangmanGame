41cc299911b97398acd5b83ff860cb72917ba574
Will <wmccrory@gmail.com>
2018-01-18 23:17:49 -0600

    Coded functional start button / Hangman image sets to starting condition on press / Word displays with hidden characters on gameboard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,54 @@
 console.log("Game is loaded");
 
 //global variables
-var wordBank = ["Judge", "Jury", "Executioner"];
-var wins = 0;
-var losses = 0;
+var wordBank = ["judge", 
+				"jury", 
+				"executioner",
+				"creep"
+				];
+//Starting lives
+	const lives = 6;
+//Lives remaining
+	var remainingLives = 0;
+//Win count
+	var wins = 0;
+//Loss count
+	var losses = 0;
 
-//Entire game written inside myFunction, executes once "Start Game" button pressed
-function myFunction() {
-	//Console message letting you know game has started running
+//Guessed letters
+	var guessedLet = [];
+//Game start condition
+	var gameStart = false;
+//Game end condition
+	var gameReset = false;
+//Secret word game progress	
+	var hiddenWord = [];
+//Starting random word from wordbank
+	var secretWord;
+
+////////////////////////////////////////////////////////////////////////////////////
+//Entire game written inside myGame, executes once "Start Game" button pressed//
+
+function myGame() {
+//Console message letting you know game has started running
 	console.log("Game is ready");
+//Starting conditions set
+	document.getElementById("lifeDisplay").innerHTML = "<h2><u>******</u></h2><h2>|***|*</h2><h2>|*****</h2><h2>|*****</h2><h2>|*****</h2><h2>|*****</h2><h2>|*****</h2>";
+	let remainingLives = lives;
+	console.log("remaining lives = " + remainingLives);
+	let gameStart = true;
+	let gameReset = false;
+	console.log("game started = " + gameStart + " game reset = " + gameReset);
 
-	document.getElementById("hiddenWord").innerHTML = "<h3><u>*</u> <u>*</u> <u>*</u> <u>*</u> <u>*</u> <u>*</u> <u>*</u></h3>";
+	secretWord = Math.floor(Math.random() * (wordBank.length));
+
+	for (var i = 0; i < wordBank[secretWord].length; i++) {
+		hiddenWord.push("<u>*</u>");
+	}
+
+	document.getElementById("hiddenWord").innerHTML = hiddenWord.join(" ");
+
+
+// //Initial game board. Ready for user input
+
 }
